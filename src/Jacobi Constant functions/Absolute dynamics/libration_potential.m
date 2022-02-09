228b3055d097ce452bf94bbb5899124d9bfff640
SergioCdV <66524268+SergioCdV@users.noreply.github.com>
2022-02-09 01:40:54 +0100

    Polynomial basis refactoring and relative Lagrangian to libration points

---
+++ src/Jacobi Constant functions/Absolute dynamics/libration_potential.m
@@ -0,0 +1,63 @@
+%% CR3BP Library %% 
+% Sergio Cuevas del Valle
+% Date: 09/02/22
+% File: libration_potential.m 
+% Issue: 0 
+% Validated: 
+
+%% Libration potential %%
+% For a given gravitational parameter mu and position vector r, this function computes the 
+% Lagrangian function associated to a given libration point. 
+
+% Inputs: - scalar mu, the reduced gravitational parameter of the system. 
+%         - vector L, the libration point and associated chracteristic
+%           distance of the system to be considered.
+%         - vector s, a 12x1 array containing the relative synodic position 
+%           velocity vectors of the chaser and the target.
+%         - scalar order, the order of the approximation in the Legendre
+%           series
+
+% Outputs: - scalar L, the Lagrangian function associated with the input state vector. 
+
+% New versions:
+
+function [L] = libration_potential(mu, L, s, order)
+    %Constants of the problem 
+    mu_r(1) = 1-mu;                     %Gravitational parameters of the most massive primary
+    mu_r(2) = mu;                       %Gravitational parameters of the least massive primary
+    
+    %Location of the unsteady primaries 
+    R(1:3,1) = [-mu; 0; 0];             %Location of the first primary
+    R(1:3,2) = [1-mu; 0; 0];            %Location of the second primary
+
+    %State variables 
+    r = s(1:3);                         %Position vector
+    v = s(4:6);                         %Velocity vector
+
+    %Relative state variables 
+    LID = L(1);                         %Identifier of the libration point
+    switch (LID)
+        case 1
+            gamma = L(end);             %Characteristic distance of the libration point
+            r(1) = r(1)-1+mu+gamma;     %Relative x coordinate to the libration point
+            r = r/gamma;                %Scaled position vector
+        case 2
+            gamma = L(end);             %Characteristic distance of the libration point
+            r(1) = r(1)-1+mu-gamma;     %Relative x coordinate to the libration point
+            r = r/gamma;                %Scaled position vector
+        otherwise
+            error('Libration point calculation has not been implemented yet');
+    end
+
+    %Total kinetic energy
+    v = v + [-r(2); r(1); 0];           %Total velocity vector    
+    T = (1/2)*dot(v,v);
+
+    %Relative potential function 
+    L = T;
+    for i = 1:length(mu_r)
+        for j = 1:order
+            L = L + (norm(r)/norm(R(:,i)))^j*legendre_polynomials(j, r(1)/norm(r));
+        end
+    end
+end
