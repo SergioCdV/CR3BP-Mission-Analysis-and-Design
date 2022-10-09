3a5cb37526849e78cbdb40343aa5c4ee36215242
SergioCdV <66524268+SergioCdV@users.noreply.github.com>
2022-10-09 18:48:39 +0200

    MISG working

--- Demonstrations/Control/MISG.m
+++ Demonstrations/Control/MISG.m
@@ -2,115 +2,98 @@
 % Sergio Cuevas del Valle % 
 % 01/04/21 % 
 
-%% GNC 1: Two-impulsive rendezvous via STM %% 
-% This script provides an interface to test the two-impusilve rendezvous strategy using the STM of 
-% the dynamical model. 
-
-% The relative motion of two spacecraft in the same halo orbit (closing and RVD phase) around L1 in the
-% Earth-Moon system is analyzed.
-
-% The first impulse is known as targetting, aimed to nullify the relative
-% position between chaser and target after some flight time tf. The second
-% impulse nullifies the relative velocity between the two to complete the
-% rendezvous.
+%% GNC 1: Multi-impulsive, staging guidance %% 
+% This script provides an interface to test the MISG guidance core. 
 
 % In the relative phase space, the relative particle is driven to the
 % origin of the synodic frame.
 
-% Units are non-dimensional and solutions are expressed in the Lagrange
-% points reference frame as defined by Howell, 1984.
+% Units are non-dimensional and solutions are expressed in the synodic 
+% reference frame as defined by Howell, 1984.
 
 %% Set up %%
-%Set up graphics 
+% Set up graphics 
 set_graphics();
 
-%Integration tolerances (ode113)
+% Integration tolerances (ode113)
 options = odeset('RelTol', 2.25e-14, 'AbsTol', 1e-22);  
 
 %% Contants and initial data %% 
-%Time span 
-dt = 1e-3;                          %Time step
-tf = 0.6;                           %Rendezvous time
-tspan = 0:dt:tf;                    %Integration time span
-tspann = 0:dt:2*pi;                 %Integration time span
+% Time span 
+dt = 1e-3;                          % Time step
+tf = 0.6;                           % Rendezvous time
+tspan = 0:dt:2*pi;                  % Integration time span
 
-%CR3BP constants 
-mu = 0.0121505;                     %Earth-Moon reduced gravitational parameter
-L = libration_points(mu);           %System libration points
-Lem = 384400e3;                     %Mean distance from the Earth to the Moon
+% CR3BP constants 
+mu = 0.0121505;                     % Earth-Moon reduced gravitational parameter
+L = libration_points(mu);           % System libration points
+Lem = 384400e3;                     % Mean distance from the Earth to the Moon
 
-%Differential corrector set up
-nodes = 10;                         %Number of nodes for the multiple shooting corrector
-maxIter = 20;                       %Maximum number of iterations
-tol = 1e-10;                        %Differential corrector tolerance
+% Differential corrector set up
+nodes = 10;                         % Number of nodes for the multiple shooting corrector
+maxIter = 20;                       % Maximum number of iterations
+tol = 1e-10;                        % Differential corrector tolerance
 
 %% Initial conditions and halo orbit computation %%
-%Halo characteristics 
-Az = 20e6;                                                  %Orbit amplitude out of the synodic plane. 
-Az = dimensionalizer(Lem, 1, 1, Az, 'Position', 0);         %Normalize distances for the E-M system
-Ln = 2;                                                     %Orbits around L1
-gamma = L(end,Ln);                                          %Li distance to the second primary
-m = 1;                                                      %Number of periods to compute
+% Halo characteristics 
+Az = 20e6;                                                  % Orbit amplitude out of the synodic plane. 
+Az = dimensionalizer(Lem, 1, 1, Az, 'Position', 0);         % Normalize distances for the E-M system
+Ln = 2;                                                     % Orbits around L1
+gamma = L(end,Ln);                                          % Li distance to the second primary
+m = 1;                                                      % Number of periods to compute
 
-%Compute a halo seed 
-halo_param = [1 Az Ln gamma m];                             %Northern halo parameters
-[halo_seed, period] = object_seed(mu, halo_param, 'Halo');  %Generate a halo orbit seed
+% Compute a halo orbit seed 
+halo_param = [1 Az Ln gamma m];                             % Northern halo parameters
+[halo_seed, period] = object_seed(mu, halo_param, 'Halo');  % Generate a halo orbit seed
 
 %Correct the seed and obtain initial conditions for a halo orbit
 [target_orbit, ~] = differential_correction('Plane Symmetric', mu, halo_seed, maxIter, tol);
 
-%Continuate the first halo orbit to locate the chaser spacecraft
-Bif_tol = 1e-2;                                             %Bifucartion tolerance on the stability index
-num = 5;                                                    %Number of orbits to continuate
-method = 'SPC';                                             %Type of continuation method (Single-Parameter Continuation)
-algorithm = {'Energy', NaN};                                %Type of SPC algorithm (on period or on energy)
-object = {'Orbit', halo_seed, period};                      %Object and characteristics to continuate
-corrector = 'Plane Symmetric';                              %Differential corrector method
-direction = 1;                                              %Direction to continuate (to the Earth)
-setup = [mu maxIter tol direction];                         %General setup
+% Continuate the first halo orbit to locate the chaser spacecraft
+Bif_tol = 1e-2;                                             % Bifucartion tolerance on the stability index
+num = 5;                                                    % Number of orbits to continuate
+method = 'SPC';                                             % Type of continuation method (Single-Parameter Continuation)
+algorithm = {'Energy', NaN};                                % Type of SPC algorithm (on period or on energy)
+object = {'Orbit', halo_seed, period};                      % Object and characteristics to continuate
+corrector = 'Plane Symmetric';                              % Differential corrector method
+direction = 1;                                              % Direction to continuate (to the Earth)
+setup = [mu maxIter tol direction];                         % General setup
 
 [chaser_seed, state_PA] = continuation(num, method, algorithm, object, corrector, setup);
 [chaser_orbit, ~] = differential_correction('Plane Symmetric', mu, chaser_seed.Seeds(end,:), maxIter, tol);
 
 %% Modelling in the synodic frame %%
-r_t0 = target_orbit.Trajectory(100,1:6);                    %Initial target conditions
-r_c0 = chaser_orbit.Trajectory(1,1:6);                      %Initial chaser conditions 
-rho0 = r_c0-r_t0;                                           %Initial relative conditions
-s0 = [r_t0 rho0].';                                         %Initial conditions of the target and the relative state
+r_t0 = target_orbit.Trajectory(600,1:6);                    % Initial target conditions
+r_c0 = chaser_orbit.Trajectory(1,1:6);                      % Initial chaser conditions 
+rho0 = r_c0-r_t0;                                           % Initial relative conditions
+s0 = [r_t0 rho0].';                                         % Initial conditions of the target and the relative state
 
-%Integration of the model
-[~, S] = ode113(@(t,s)nlr_model(mu, true, false, false, 'Encke', t, s), tspann, s0, options);
+% Integration of the model
+[~, S] = ode113(@(t,s)nlr_model(mu, true, false, false, 'Encke', t, s), tspan, s0, options);
 Sn = S;                
 
-%Reconstructed chaser motion 
-S_rc = S(:,1:6)+S(:,7:12);                                  %Reconstructed chaser motion via Encke method
+% Reconstructed chaser motion 
+S_rc = S(:,1:6)+S(:,7:12);                                  % Reconstructed chaser motion via Encke method
 
-%% GNC: two impulsive rendezvous, single shooting scheme %%
-%Differential corrector scheme
-tol = 1e-10;                                                        %Differential corrector tolerance
+%% GNC: MISG guidance %%
+% Scheme setup
+tol = [1e-8 1e-6];      % Convergence toleranes 
+N = 100;                % Number of impulses
+method = 'MPC';         % Solver method                                
 
+% Controller scheme
 tic
-[St, dV, state] = MISG_control(mu, tf, s0, tol);  %Controller scheme
+[tspan, St, dV, state] = MISG_control(mu, 1, s0, method, N, tol);  
 toc
 
-%Total maneuver metrics 
+% Total maneuver metrics 
 effort = control_effort(tspan, dV, true);
 
-%Error in time 
-[e, merit] = figures_merit(tspan, St);
+% Error in time 
+[e, merit] = figures_merit(tspan, St(:,7:12));
 
 %% Results %% 
-%Print results 
-disp('SIMULATION RESULTS: ')
-if (state.State)
-    disp('   Two impulsive rendezvous was achieved');
-    fprintf('   Delta V budget (L1 norm): %.4ei %.4ej %.4ek \n', effort(:,2));
-    fprintf('   Delta V budget (L2 norm): %.4ei %.4ej %.4ek \n', effort(:,1));
-else
-    disp('    Two impulsive rendezvous was not achieved');
-end
-
-%Plot results 
+% Plot results 
 figure(1) 
 view(3) 
 hold on
@@ -118,23 +101,21 @@ plot3(Sn(:,1), Sn(:,2), Sn(:,3));
 plot3(S_rc(:,1), S_rc(:,2), S_rc(:,3)); 
 hold off
 legend('Target motion', 'Chaser motion'); 
-xlabel('Synodic $x$ coordinate');
-ylabel('Synodic $y$ coordinate');
-zlabel('Synodic $z$ coordinate');
+xlabel('$x$');
+ylabel('$y$');
+zlabel('$z$');
 grid on;
-title('Reconstruction of the natural chaser motion');
 
-%Plot relative phase trajectory
+% Plot relative phase trajectory
 figure(2) 
 view(3) 
 plot3(St(:,7), St(:,8), St(:,9)); 
-xlabel('Synodic $x$ coordinate');
-ylabel('Synodic $y$ coordinate');
-zlabel('Synodic $z$ coordinate');
+xlabel('$x$');
+ylabel('$y$');
+zlabel('$z$');
 grid on;
-title('Motion in the relative configuration space');
 
-%Configuration space evolution
+% Configuration space evolution
 figure(3)
 subplot(1,2,1)
 hold on
@@ -142,30 +123,27 @@ plot(tspan, St(:,7));
 plot(tspan, St(:,8)); 
 plot(tspan, St(:,9)); 
 hold off
-xlabel('Nondimensional epoch');
-ylabel('Relative configuration coordinates');
+xlabel('$t$');
+ylabel('$\mathbf{\rho}$');
 grid on;
 legend('$x$', '$y$', '$z$');
-title('Relative position in time');
 subplot(1,2,2)
 hold on
 plot(tspan, St(:,10)); 
 plot(tspan, St(:,11)); 
 plot(tspan, St(:,12)); 
 hold off
-xlabel('Nondimensional epoch');
-ylabel('Relative velocity coordinates');
+xlabel('$t$');
+ylabel('$\dot{\mathbf{\rho}}$');
 grid on;
 legend('$\dot{x}$', '$\dot{y}$', '$\dot{z}$');
-title('Relative velocity in time');
 
-%Configuration space error 
+% Configuration space error 
 figure(4)
 plot(tspan, log(e)); 
-xlabel('Nondimensional epoch');
+xlabel('$t$');
 ylabel('Absolute error $\log{e}$');
 grid on;
-title('Absolute rendezvous error in the configuration space');
 
 figure(5)
 view(3) 
@@ -176,12 +154,11 @@ t = plot3(Sn(:,1), Sn(:,2), Sn(:,3), 'r', 'Linewidth', 0.1);
 scatter3(L(1,Ln), L(2,Ln), 0, 'k', 'filled');
 text(L(1,Ln), L(2,Ln), 5e-3, '$L_2$');
 hold off
-xlabel('Synodic $x$ coordinate');
-ylabel('Synodic $y$ coordinate');
-zlabel('Synodic $z$ coordinate');
+xlabel('$x$');
+ylabel('$y$');
+zlabel('$z$');
 grid on;
-legend('Initial orbit', 'Rendezvous arc', 'Target orbit', 'Location', 'northeast');
-title('Converged rendezvous trajectory in the absolute configuration space');
+legend('Initial orbit', 'Guidance trajectory', 'Target orbit', 'Location', 'northeast');
 
 %Rendezvous animation 
 if (false)
--- src/GNC algorithms/Control/Impulsive/MISG_control.m
+++ src/GNC algorithms/Control/Impulsive/MISG_control.m
@@ -13,16 +13,19 @@
 %         - scalar TOF, the time of flight for the rendezvous condition
 %         - vector s0, initial conditions of both the target and the
 %           relative particle
+%         - string method, the solver to be used
+%         - scalar N, the number of impulses in the sequence
 %         - scalar tol, the differential corrector scheme absolute
 %           tolerance
 
-% Output: - array Sc, the rendezvous relative trajectory
+% Output: - vector tspan, the sequence times
+%         - array Sc, the rendezvous relative trajectory
 %         - array dV, containing  column-wise the required number of impulses
 %         - structure state, with some metrics about the scheme performance
 
 % New versions: 
 
-function [Sc, dV, state] = MISG_control(mu, TOF, s0, tol)
+function [tspan, Sc, dV, state] = MISG_control(mu, TOF, s0, method, N, tol)
     % Constants 
     m = 6;                      % Phase space dimension
     B = [zeros(3); eye(3)];     % Control input matrix
@@ -34,103 +37,213 @@ function [Sc, dV, state] = MISG_control(mu, TOF, s0, tol)
         
     % Integration tolerance and setup 
     options = odeset('RelTol', 2.25e-14, 'AbsTol', 1e-22); 
-    
-    dt = 1e-2;                                         % Integration time step  
-    tspan = 0:dt:TOF;                                  % Integration time span
-    
-    % Differential corrector set up
-    maxIter = 100;                                     % Maximum number of iterations
-    GoOn(1) = true;                                    % Convergence boolean 
-    iter(1) = 1;                                       % Initial iteration 
-        
-    % Preallocation of the impulses 
-    dV = zeros(length(tspan),1);                       % Vector of impulses
-    u = zeros(3,length(tspan));                        % Thrusting direction
-    error = zeros(length(tspan),1);                    % Constraint vector
-    dVc = 10*ones(size(dV));                           % Converged impulse sequence
 
-    % Initial guess based on the TISS solution
-    [~, dv, ~] = TISS_control(mu, TOF, s0, tol, 'Position', true);
-    dV = repmat(1e-1, length(tspan), 1);
-    dV(end) = norm(dv(:,end));
-              
-    s0 = [s0 reshape(eye(m), [1 m^2])];
-    [~, S] = ode113(@(t,s)nlr_model(mu, true, false, true, 'Encke', t, s), tspan, s0+[zeros(1,m+m/2) dv(:,1).' zeros(1,m^2)], options);
-    S(end,10:12) = S(end,10:12)+dv(:,end).';
-    
-    % Outer loop
-    while (GoOn(1) && iter(1) < maxIter) 
-        % Inner loop constants 
-        M = reshape(S(end,2*m+1:end), [m m]); 
-
-        % Initial error 
-        epsilon = -S(end,m+1:2*m).';
-
-        % Re-initialization
-        GoOn(2) = true; 
-        iter(2) = 1; 
-
-        % Inner loop (optimzing planning) 
-        while (GoOn(2) && iter(2) < maxIter)
-            % Compute the Lagrange multiplier 
-            STM = zeros(m); 
-            for i = 1:length(dV)
-                Phi = M*reshape(S(i,2*m+1:end), [m m])^(-1);
-                STM = STM + dV(i)*Phi*B*(B.'*Phi.');
-            end
-            lambda = exp(sum(dV))*STM\epsilon; 
-
-            % Compute the thrusting directions 
-            u(:,1) = B.'*M.'*lambda/exp(sum(dV));
-            for i = 1:length(dV)-1
-                Phi0 = (M*reshape(S(i,2*m+1:end), [m m])^(-1)*B).';
-                Phi =  (M*reshape(S(i+1,2*m+1:end), [m m])^(-1)*B).';
-                u(:,i+1) = dV(i+1)/dV(i)*Phi*pinv(Phi0)*u(:,i);
-            end
-
-            % Compute the error
-            for i = 1:length(dV)
-                Phi = M*reshape(S(i,2*m+1:end), [m m])^(-1)*B;
-                error(i) = exp(sum(dV))-dot(u(:,i), Phi.'*lambda);
-            end
-
-            % Compute the Newton update
-            ds = -error / exp(sum(dV)); 
-
-            % Update the velocity norms 
-            dV = dV + ds;
-
-            % Convergence
-            if (norm(ds) < tol)
-                GoOn = false; 
-            else
-                iter(2) = iter(2) + 1; 
-            end
-        end
-        
-        % Re-integrate the trajectory
-        for i = 1:size(dV,1)               
-            % New trajectory
-            U = [zeros(1,m+m/2) dV(i)*u(:,i).' zeros(1,m^2)];
-            [~, s] = ode113(@(t,s)nlr_model(mu, true, false, true, 'Encke', t, s), [0 dt], S(i,:)+U, options); 
-
-            % Update the state trajectory
-            S(i+1,:) = s(end,:);
-        end
-
-        % Convergence analysis 
-        if (norm(dVc-dV) < tol)
-            GoOn(1) = false;                          % Stop the method
-        else
-            dVc = dV;                                 % Converged impulse sequence
-            iter(1) = iter(1)+1;                      % Update the iterations
-        end
-    end
+    tspan = linspace(0,TOF,N+1);                       % Integration time span
+    dt = tspan(2)-tspan(1);                            % Time step
             
-    % Output       
-    dV = dV.*u;                                       % Converged final impulses
-    Sc = S;                                           % Control trajectory 
-    state.State = ~ (GoOn(1) && GoOn(2));             % Convergence boolean
-    state.Iterations = max(iter);                     % Number of required iterations
-    state.Error = norm(error);                        % Final error
+    % Preallocation of the impulses     
+    dV = zeros(3*length(tspan),1);                     % Sequence of impulses
+    sol = dV; 
+
+    % Initial guess (null control)
+    s0 = [s0 reshape(eye(m), [1 m^2])];
+    [~, S] = ode113(@(t,s)nlr_model(mu, true, false, true, 'Encke', t, s), tspan, s0, options);
+
+    switch (method)
+        case 'MPC'
+            s = S;                                              % Initial guess
+            for i = 1:length(tspan)-1
+                % Constants of the loop 
+                M = reshape(s(end,2*m+1:end), [m m]);           % Final STM
+                epsilon = -s(end,m+1:2*m).';                    % Rendezvous error
+        
+                % Solve the optimal problem
+                aux = fsolve(@(x)optimal_sequence(s(i:end,:),M,B,epsilon,0.1,x), sol(1+3*(i-1):end), optimoptions('fsolve', 'Display', 'off', 'Algorithm', 'levenberg-marquardt'));
+                sol(1+3*(i-1):3*i) = aux(1:3);
+        
+                % Update the propagation 
+                U = [zeros(1,m+m/2) sol(1+3*(i-1):3*i).' zeros(1,m^2)];
+                [~, aux] = ode113(@(t,s)nlr_model(mu, true, false, true, 'Encke', t, s), tspan(i:end), s(i,:)+U, options); 
+                if (length(tspan(i:end)) == 2)
+                    s(i:end,:) = aux([1 end], :);
+                else
+                    s(i:end,:) = aux;
+                end
+            end  
+
+            sol(end-2:end) = -s(end,10:12).';                 % Final impulse 
+            s(end,10:12) = zeros(1,3);
+
+            % Output
+            dV = reshape(sol, [3 length(sol)/3]);             % Converged final impulses
+            Sc = s;                                           % Control trajectory 
+            state.State = true;                               % Convergence boolean
+            state.Iterations = length(tspan);                 % Number of required iterations
+            state.Error = norm(epsilon);                      % Final error
+
+        case 'Numerical'
+            % Differential corrector set up
+            maxIter = 100;                                    % Maximum number of iterations
+            GoOn(1) = true;                                   % Convergence boolean 
+            iter(1) = 1;                                      % Initial iteration 
+
+            % Outer loop
+            while (GoOn(1) && iter(1) < maxIter) 
+                % Inner loop constants 
+                M = reshape(S(end,2*m+1:end), [m m]);         % Final STM
+                epsilon = -S(end,m+1:2*m).';                  % Rendezvous error
+                        
+                % Inner loop (numerical solver) 
+                sol = fsolve(@(x)optimal_sequence(S,M,B,epsilon,0.1,x), sol, optimoptions('fsolve', 'Display', 'off', 'Algorithm', 'levenberg-marquardt'));
+                
+                % Re-integrate the trajectory
+                for i = 1:size(S,1)               
+                    % New trajectory
+                    U = [zeros(1,m+m/2) sol(1+m/2*(i-1):m/2*i).' zeros(1,m^2)];
+                    [~, s] = ode113(@(t,s)nlr_model(mu, true, false, true, 'Encke', t, s), [0 dt], S(i,:)+U, options); 
+        
+                    % Update the state trajectory
+                    S(i+1,:) = s(end,:);
+                end
+                S = S(1:end-1,:);
+        
+                % Convergence analysis 
+                if (norm(dV-sol(1:m/2*size(S,1))) < tol)
+                    GoOn(1) = false;                          % Stop the method
+                else
+                    dV = sol(1:m/2*size(S,1));                % Converged impulse sequence
+                    iter(1) = iter(1)+1;                      % Update the iterations
+                end
+            end
+
+            % Output
+            dV = reshape(sol, [3 length(sol)/3]);             % Converged final impulses
+            Sc = S;                                           % Control trajectory 
+            state.State = ~GoOn(1);                           % Convergence boolean
+            state.Iterations = iter;                          % Number of required iterations
+            state.Error = norm(epsilon);                      % Final error
+
+        case 'Sequential'
+            % Differential corrector set up
+            maxIter = 100;                                    % Maximum number of iterations
+            GoOn(1) = true;                                   % Convergence boolean 
+            iter(1) = 1;                                      % Initial iteration 
+
+            % Outer loop
+            while (GoOn(1) && iter(1) < maxIter) 
+                % Inner loop constants 
+                M = reshape(S(end,2*m+1:end), [m m]);         % Final STM
+                epsilon = -S(end,m+1:2*m).';                  % Rendezvous error
+                        
+                % Inner loop (numerical solver) 
+                if (iter(1) == 1)
+                    sol = fsolve(@(x)optimal_sequence(S,M,B,epsilon,0.1,x), sol, optimoptions('fsolve', 'Display', 'off', 'Algorithm', 'levenberg-marquardt'));
+                end
+
+                GoOn(2) = true; 
+                iter(2) = 1; 
+
+                while (GoOn(2) && iter(2) < maxIter)
+                    % Sequential update 
+                    seq_eq = @(A,V0,x) (norm(V0)*x-A*norm(x)*V0);
+                    for i = 1:length(tspan)-1
+                        Phi0 = (M*reshape(S(i,2*m+1:end), [m m])^(-1)*B).';
+                        Phi1 = (M*reshape(S(i+1,2*m+1:end), [m m])^(-1)*B).'; 
+                        A = Phi1*pinv(Phi0);
+                        sol(1+3*i:3*(i+1)) = fsolve(@(x)seq_eq(A, sol(1+3*(i-1):3*i), x), sol(1+3*i:3*(i+1)), optimoptions('fsolve', 'Display', 'off', 'Algorithm', 'levenberg-marquardt'));
+                    end
+
+                    % Initial impulse update
+                    STM = zeros(m,m/2);             % Compute the complete STM
+                    for i = 1:length(tspan)
+                        % Initial STM
+                        Phi0 = (M*reshape(S(1,2*m+1:end), [m m])^(-1)*B).';
+                        Phi1 = (M*reshape(S(2,2*m+1:end), [m m])^(-1)*B).'; 
+                        Prod = Phi1*pinv(Phi0);
+
+                        % Cumulative STM
+                        for j = 2:i-1
+                            Phi0 = (M*reshape(S(j,2*m+1:end), [m m])^(-1)*B).';
+                            Phi1 = (M*reshape(S(j+1,2*m+1:end), [m m])^(-1)*B).'; 
+                            Prod = Phi1*pinv(Phi0)*Prod;
+                        end 
+                        Phi = M*reshape(S(i,2*m+1:end), [m m])^(-1);
+                        stm = norm(sol(1+3*(i-1):3*i))*Phi*B*Prod;
+                        STM = STM+stm;
+                    end
+                    e = STM*sol(1:3)/norm(sol(1:3))-epsilon;
+                    G = STM*(eye(3)-sol(1:3)*sol(1:3).')/norm(sol(1:3))^3;
+
+                    % Newton update 
+                    ds = -pinv(G)*e;
+                    sol(1:3) = sol(1:3)+ds;
+
+                    % Convergence analysis 
+                    if (norm(ds) < tol(1))
+                        GoOn(2) = false;
+                    else
+                        iter(2) = iter(2)+1;
+                    end
+                end
+                
+                % Re-integrate the trajectory
+                for i = 1:size(S,1)               
+                    % New trajectory
+                    U = [zeros(1,m+m/2) sol(1+m/2*(i-1):m/2*i).' zeros(1,m^2)];
+                    [~, s] = ode113(@(t,s)nlr_model(mu, true, false, true, 'Encke', t, s), [0 dt], S(i,:)+U, options); 
+        
+                    % Update the state trajectory
+                    S(i+1,:) = s(end,:);
+                end
+                S = S(1:end-1,:);
+        
+                % Convergence analysis 
+                if (norm(dV-sol(1:m/2*size(S,1))) < tol(2))
+                    GoOn(1) = false;                          % Stop the method
+                else
+                    dV = sol(1:m/2*size(S,1));                % Converged impulse sequence
+                    iter(1) = iter(1)+1;                      % Update the iterations
+                end
+            end
+
+            sol(end-2:end) = -s(end,10:12).';                 % Final impulse 
+            S(end,10:12) = zeros(1,3);
+
+            % Output
+            dV = reshape(sol, [3 length(sol)/3]);             % Converged final impulses
+            Sc = S;                                           % Control trajectory 
+            state.State = ~GoOn(1);                           % Convergence boolean
+            state.Iterations = iter;                          % Number of required iterations
+            state.Error = norm(epsilon);                      % Final error
+
+        otherwise
+            error('No valid method was chosen');
+    end
+end
+
+%% Auxiliary functions 
+function [dV] = optimal_sequence(S, M, B, epsilon, dVmax, x)
+    % State variables
+    V = reshape(x(1:3*size(S,1)), [3 size(S,1)]);           % Optimal sequence
+    % t = x(3*size(S,1)+1:end);                             % Slack variable
+
+    % Optimal thrusting direction
+    dV = zeros(3*(size(S,1)-1),1);
+    for i = 1:size(S,1)-1
+        Phi0 = M*reshape(S(i,13:end), [6 6])^(-1)*B;
+        Phi1 = M*reshape(S(i+1,13:end), [6 6])^(-1)*B;
+        dV(1+3*(i-1):3*i,1) = norm(V(:,i))*V(:,i+1)-norm(V(:,i+1))*Phi1.'*pinv(Phi0.')*V(:,i);
+    end
+
+    % Rendezvous constraint
+    m = 6;
+    Aeq = zeros(6,3*size(S,1));
+    M = reshape(S(end,2*m+1:end), [m m]);
+    for i = 1:size(S,1)
+        Aeq(1:m,1+m/2*(i-1):m/2*i) = M*reshape(S(i,2*m+1:end), [m m])^(-1)*B;
+    end
+    dV = [dV; Aeq*x(1:3*size(S,1))-epsilon];
+
+    % Maximum impulse inequality
+    % dV = [dV; dVmax^2-dot(V,V,1).'-t.^2];
 end
